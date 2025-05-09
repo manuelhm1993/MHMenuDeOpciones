e4d1d6229d041d6470979d66f9e65a753106cb50
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-09 12:30:08 -0400

    Agregar archivos de proyecto.

---
+++ MHMenuDeOpciones/App/Src/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MHMenuDeOpciones.App.Src.Views;
+
+namespace MHMenuDeOpciones.App.Src.Controllers
+{
+    public static class MenuController
+    {
+        #region Campos de clase
+        private static MenuView _FormMenu = new MenuView();
+        #endregion
+
+        #region Propiedades
+        public static MenuView FormMenu
+        {
+            get => _FormMenu;
+            set => _FormMenu = value;
+        }
+        #endregion
+
+        #region Métodos
+        public static MenuView GetGUIMenuOpciones() => FormMenu;
+        #endregion
+    }
+}
---
+++ MHMenuDeOpciones/App/Src/Views/MenuView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MHMenuDeOpciones.App.Src.Views
+{
+    public partial class MenuView : Form
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
